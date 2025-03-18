18b9b38942120f583383f3c28af221d5bd11efa6
Christian Manukyan <manukyanchristian@gmail.com>
2025-03-19 01:22:49 +0400

    simulate horizontal scaling with Docker Compose and make NATS to work with queue

--- apps/workflows-service/src/workflows-service.module.ts
+++ apps/workflows-service/src/workflows-service.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { WorkflowsServiceController } from './workflows-service.controller';
-import { WorkflowsServiceService } from './workflows-service.service';
 import { WorkflowsModule } from './workflows/workflows.module';
 import { HealthModule } from './health/health.module';
 
@@ -20,7 +18,5 @@ import { HealthModule } from './health/health.module';
     WorkflowsModule,
     HealthModule,
   ],
-  controllers: [WorkflowsServiceController],
-  providers: [WorkflowsServiceService],
 })
 export class WorkflowsServiceModule {}
--- apps/workflows-service/src/workflows/workflows.service.ts
+++ apps/workflows-service/src/workflows/workflows.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, Repository } from 'typeorm';
 import { Workflow } from './entities/workflow.entity';
@@ -6,6 +6,8 @@ import { CreateWorkflowDto, UpdateWorkflowDto } from '@app/workflows';
 
 @Injectable()
 export class WorkflowsService {
+  private readonly logger = new Logger(WorkflowsService.name);
+
   constructor(
     @InjectRepository(Workflow)
     private readonly workflowsRepository: Repository<Workflow>,
@@ -29,6 +31,10 @@ export class WorkflowsService {
     } as DeepPartial<Workflow>);
 
     const newWorkflowEntity = await this.workflowsRepository.save(workflow);
+    this.logger.debug(
+      `Created workflow with id ${newWorkflowEntity.id} for building ${newWorkflowEntity.buildingId}`,
+    );
+
     return newWorkflowEntity;
   }
 
